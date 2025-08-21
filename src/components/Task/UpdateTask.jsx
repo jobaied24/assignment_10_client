df9046f0aa0cc6e7e1a8988dcd4de4755e95c42d
hero00 <jobaied543@gmail.com>
2025-08-21 15:14:00 +0600

    completed update task

--- src/components/Task/UpdateTask.jsx
+++ src/components/Task/UpdateTask.jsx
@@ -4,36 +4,57 @@ import { useLoaderData } from 'react-router';
 const UpdateTask = () => {
     const data=useLoaderData();
     console.log(data);
+        const {name,email,title,category,deadline,description,budget}=data;
+
+
+    const handleUpdateTask=e=>{
+        e.preventDefault();
+        const form=e.target;
+        const formData=new FormData(form);
+        const updateData=Object.fromEntries(formData.entries());
+
+        fetch(`http://localhost:3000/task/${data._id}`,{
+            method:'PUT',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(updateData)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+        })
+    }
     return (
         <div>
             <h1 className='my-10 text-center text-4xl font-bold '>Update Task</h1>
         
-         <form>
+         <form onSubmit={handleUpdateTask}>
         <div className='my-10'>
           <div className='mx-10 grid md:grid-cols-2 gap-5'>
 
             {/* title */}
             <fieldset className="fieldset grid md:grid-cols bg-gray-300 border-base-300 rounded-box border p-4">
               <label className="label text-gray-600">Task title</label>
-              <input type="text" name='title' className="input w-full" placeholder="Task title" />
+              <input type="text" name='title' defaultValue={title} className="input w-full" placeholder="Task title" />
             </fieldset>
 
             {/* Category */}
             <fieldset className="fieldset grid md:grid-cols bg-gray-300 border-base-300 rounded-box border p-4">
               <label className="label text-gray-600">Category</label>
-              <input type="text" name='category' className="input w-full" placeholder="Category" />
+              <input type="text" name='category' defaultValue={category} className="input w-full" placeholder="Category" />
             </fieldset>
 
             {/* Deadline */}
             <fieldset className="fieldset grid md:grid-cols bg-gray-300 border-base-300 rounded-box border p-4">
               <label className="label text-gray-600">Deadline</label>
-              <input type="date" name='deadline' className="input w-full" placeholder="Deadline" />
+              <input type="date" name='deadline' defaultValue={deadline} className="input w-full" placeholder="Deadline" />
             </fieldset>
 
             {/* Budget */}
             <fieldset className="fieldset grid md:grid-cols bg-gray-300 border-base-300 rounded-box border p-4">
               <label className="label text-gray-600">Budget</label>
-              <input type="text" name='budget' className="input w-full" placeholder="Budget" />
+              <input type="text" name='budget' defaultValue={budget} className="input w-full" placeholder="Budget" />
             </fieldset>
 
             {/* Name  */}
@@ -54,7 +75,7 @@ const UpdateTask = () => {
             {/* Description  */}
             <fieldset className="fieldset grid md:grid-cols bg-gray-300 border-base-300 rounded-box border p-4">
               <label className="label text-gray-600">Description</label>
-              <input type="text" name='description' className="input w-full" placeholder="Description" />
+              <input type="text" name='description' defaultValue={description} className="input w-full" placeholder="Description" />
             </fieldset>
 
             {/* Update Task */}
